6b353c0054ed7b4be2c00b64b21661eb3c974005
Aarón Ulloa <simiuc00@gmail.com>
2022-08-16 22:02:40 -0500

    Subo archivos del dia 16-08-2022

---
+++ java/ejemplojdk/Hola.java
@@ -0,0 +1,15 @@
+public class Hola{
+//esto es una clase del JDK java
+
+  //String miVariable = "Hola mundo java";
+  
+	 public static void main (String [] arg){
+	 //String miVariable = "Hola mundo saludo a:"
+       String miNombre = "Simi Ulloa Cedillo";
+	 // esta es la declaracion del metodo main
+			  System.out.print ("Hola saludos a:" + miNombre);
+
+	 }
+
+}
+
---
+++ java/intelli/prueba3/src/main/java/Holamundo.java
@@ -0,0 +1,7 @@
+public class Holamundo {
+    public static void main(String[] args) {
+        //String miVariable = "Hola mundo java";
+        String nombre = "Simi";
+        System.out.println("Saludos desde java a:" + nombre);
+    }
+}
