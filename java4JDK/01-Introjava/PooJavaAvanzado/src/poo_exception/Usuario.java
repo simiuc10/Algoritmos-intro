3be1e4b5d9e7044880999a0773859ae220563da4
Aarón Ulloa <simiuc00@gmail.com>
2022-09-01 18:43:02 -0500

    subo archivos del dia 01-09-2022

---
+++ java4JDK/01-Introjava/PooJavaAvanzado/src/poo_exception/Usuario.java
@@ -0,0 +1,40 @@
+package poo_exception;
+
+public class Usuario {
+    private String usrname;
+    private String password;
+    private String nombre;
+    private String email;
+
+    public String getUsrname() {
+        return usrname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsrname(String usrname) {
+        this.usrname = usrname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
